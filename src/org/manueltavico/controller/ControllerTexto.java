fa59142bf65acc94780f6de71e1ce3ee9eb9eb93
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-06-10 04:45:58 +0200

    006 se agregaron  jercicios de texto

--- src/org/manueltavico/controller/ControllerTexto.java
+++ src/org/manueltavico/controller/ControllerTexto.java
@@ -9,5 +9,36 @@ public class ControllerTexto {
     public String concatenadorDeNombre
         (String nombre, String apellido) { return nombre + " " + apellido; 
     }
+     
+    public int contadorDeLongitud
+        (String palabra) { return palabra.length(); 
+        }
         
+    public String inversorDeTexto
+        (String palabra) { return new StringBuilder(palabra).reverse().toString(); 
+        }
+        
+    public boolean detectorDeVocales
+        (char caracter) { return "aeiouAEIOU".indexOf(caracter) != -1; 
+        }
+        
+    public String extractorDeIniciales
+        (String nombre, String apellido) { return "" + nombre.charAt(0) + apellido.charAt(0); 
+        }
+        
+    public String normalizadorAMayusculas
+        (String texto) { return texto.toUpperCase();
+        }
+        
+    public String conversorBinario
+        (String binario) { return (binario.length() % 2 == 0) ? "Par" : "Impar";
+        }
+        
+    public String repetidorDePalabras
+        (String palabra, int n) { return palabra.repeat(n);
+    }
+        
+    public String validadorDePassword
+        (String password) { return (password.length() > 8) ? "Segura" : "Insegura";
+        }    
 }
