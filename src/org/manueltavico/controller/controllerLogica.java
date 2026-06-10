60e49180414f5f2fdbcbf2a9489631e890347f87
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-06-10 04:49:56 +0200

    008 se agrego primer ejercicio de logica

--- src/org/manueltavico/controller/controllerLogica.java
+++ src/org/manueltavico/controller/controllerLogica.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.manueltavico.controller;
 
-/**
- *
- * @author informatica
- */
+
 public class controllerLogica {
     
-
+public boolean rangoNumerico
+        (int numero, int min, int max) { return numero >= min && numero <= max;
+}
     
 }
