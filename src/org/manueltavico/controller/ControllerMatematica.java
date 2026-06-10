245fe114c19490e6d64013b83bcc4ab6863fd731
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-06-10 04:40:27 +0200

    004 se agregaron el resto de opciones matematicas

--- src/org/manueltavico/controller/ControllerMatematica.java
+++ src/org/manueltavico/controller/ControllerMatematica.java
@@ -20,5 +20,27 @@ public class ControllerMatematica {
         (double c) { return (c * 9 / 5) + 32; 
     }
     
+        public double perimetroCirculo
+        (double r) { return 2 * Math.PI * r; 
+        }
+        
+    public double volumenCilindro
+        (double r, double h) { return Math.PI * Math.pow(r, 2) * h; 
+        }
+        
+    public double hipotenusa
+        (double a, double b) { return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        }
+        
+    public int diasAMinutos
+        (int d) { return d * 24 * 60;
+        }
+    public int restoDivision
+        (int n1, int n2) { return n1 % n2;
+        }
+        
+    public int potenciaCubo
+        (int n) { return (int) Math.pow(n, 3);
+    }
 }
 
