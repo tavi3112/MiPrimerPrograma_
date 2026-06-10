07b3f74c378eef3bd7bb143c1cf5f526210f910b
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-06-10 04:55:09 +0200

    009 se agregaron ejercicios de logica

--- src/org/manueltavico/controller/controllerLogica.java
+++ src/org/manueltavico/controller/controllerLogica.java
@@ -4,8 +4,43 @@ package org.manueltavico.controller;
 
 public class controllerLogica {
     
-public boolean rangoNumerico
-        (int numero, int min, int max) { return numero >= min && numero <= max;
+public boolean verificadorDeEdad
+        (int edad) { return edad >= 18;
 }
+
+    public boolean numeroPar
+        (int numero) { return numero % 2 == 0; 
+    }
     
+    public String semaforo(String color) { 
+        if (color.equalsIgnoreCase("rojo")) return "Detenerse";
+        if (color.equalsIgnoreCase("amarillo")) return "Precaución";
+        return "Avanzar";
+    }
+    
+    public boolean rangoNumerico
+        (int numero, int min, int max) { return numero >= min && numero <= max; 
+    }
+        
+    public boolean loginSimple
+        (String usuario, String pass) { return usuario.equals("admin") && pass.equals("1234");
+        }
+        
+    public int mayorDeDos
+        (int n1, int n2) { return (n1 > n2) ? n1 : n2; 
+    }
+        
+    public boolean añoBisiesto
+        (int anio) { return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+        }
+        
+    public boolean aprobadoReprobado
+        (double nota) { return nota >= 60;
+        }
+        
+    public boolean validadorDeTriangulo
+        (double l1, double l2, double l3) { return (l1 + l2 > l3) && (l1 + l3 > l2) && (l2 + l3 > l1); 
+        }
+        
+        
 }
